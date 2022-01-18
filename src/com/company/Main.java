68825bd897e04a46a400872b88bff1edc05b7ff9
Aleksi <a12345@live.fi>
2022-01-18 22:04:23 +0200

    Refactoring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,26 +8,4 @@ public class Main {
 	    //int[] numbers = {1,4,10};
         //Myclass2.smallestAmountBetweenInArray(numbers);
     }
-
-    /*public static final int smallestAmountBetweenInArray(int[] numArray) {
-        int minDistance = Integer.MAX_VALUE;
-        int numbersBetween;
-
-        Arrays.sort(numArray);
-
-        numArray = Arrays.stream(numArray).distinct().toArray();
-
-        for(int i = 1; i < numArray.length; i++) {
-            int diff = numArray[i] - numArray[i - 1];
-            if(diff < minDistance) {
-                minDistance = diff;
-            }
-        }
-
-        System.out.println("etäisyys: " + minDistance);
-
-        numbersBetween = minDistance - 1;
-        System.out.println("montaNumeroaMahtuu: " + numbersBetween);
-        return minDistance;
-    }*/
 }
